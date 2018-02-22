9e0937a93adb92e870ec8422253b691307c16f0a
ssh24 <sshasan10@hotmail.com>
2018-02-21 19:14:29 -0500

    Initialize test structure

---
+++ src/BORGCalendar/common/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import net.sf.borg.common.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EECS4313A2AllBlackBoxTests {
+
+	@Test
+	public void testIsAfter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetToMidnight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDayOfEpoch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMinuteString() {
+		fail("Not yet implemented");
+	}
+
+}
