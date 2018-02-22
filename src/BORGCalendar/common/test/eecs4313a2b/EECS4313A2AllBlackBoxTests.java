69a5f46c87e9ac556e3444dc7d32c9a934a19be6
ssh24 <sshasan10@hotmail.com>
2018-02-21 19:14:29 -0500

    Complete Decision Table Testing for Blackbox

---
+++ src/BORGCalendar/common/test/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -0,0 +1,32 @@
+package eecs4313a2b;
+
+import static org.junit.Assert.*;
+import java.util.Date;
+import net.sf.borg.common.*;
+import org.junit.Test;
+
+public class EECS4313A2AllBlackBoxTests {
+
+	@Test
+	public void testIsAfter() {
+		/** Method used: Decision Table Testing **/
+		
+		Date d1 = new Date(117, 11, 3);
+		Date d2 = new Date(117, 11, 3);
+		boolean result;
+		
+		// date d1 is equal to d2
+		result = DateUtil.isAfter(d1, d2);
+		assertFalse("Date d1 is equal to d2", result);
+		
+		// date d1 is before d2
+		d1.setDate(2);
+		result = DateUtil.isAfter(d1, d2);
+		assertFalse("Date d1 is before d2", result);
+		
+		// date d1 is after d2
+		d1.setDate(4);
+		result = DateUtil.isAfter(d1, d2);
+		assertTrue("Date d1 is after d2", result);
+	}
+}
