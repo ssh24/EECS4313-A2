9367bc458c938df33ebbfc462a396cac79b54678
ssh24 <sshasan10@hotmail.com>
2018-02-27 19:29:24 -0500

    Init white box testing

---
+++ AutoTest/eecs4313a2w/EECS4313A2AllWhiteBoxTests.java
@@ -0,0 +1,29 @@
+package eecs4313a2w;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EECS4313A2AllWhiteBoxTests {
+
+	@Test
+	public void testIsAfter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetToMidnight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDayOfEpoch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMinuteString() {
+		fail("Not yet implemented");
+	}
+
+}
