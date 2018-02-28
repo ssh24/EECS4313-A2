4f5394bc47c08368b6845941709cdafaf22e084c
eddyv <edward.eddy.vaisman@gmail.com>
2018-02-28 13:27:00 -0500

    bvt

--- src/BORGCalendar/common/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
+++ src/BORGCalendar/common/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -1,20 +1,37 @@
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import net.sf.borg.common.SocketClient;
+import net.sf.borg.common.SocketHandler;
+import net.sf.borg.common.SocketServer;
 
-public class EECS4313A2AllBlackBoxTests {
+public class EECS4313A2AllBlackBoxTests implements SocketHandler {
+
+	/**
+	 * process a socket message
+	 */
+	@Override
+	public synchronized String processMessage(String msg) {
+		return "";
+	}
 
 	@Test
 	public void testIsAfter() {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void test_sendMsg() {
-		/** Method used: Boundary Value Testing **/
+	@Before
+	public void setUp_sendMsg() {
 
+	}
+
+	@Test
+	public void test_sendMsg() throws IOException {
+		/** Method used: Boundary Value Testing **/
 		String validHost = "localhost";
 		String invalidHost = "asdfasdf";
 		String msg = "Port 2929";
@@ -29,39 +46,44 @@ public class EECS4313A2AllBlackBoxTests {
 		int port_min_minus = -1; // x_min-
 		int port_max_plus = 65536; // x_max_+
 
+		String response = "";
 		// port_norm
-		String response = SocketClient.sendMsg(invalidHost, port_norm, msg);
+		SocketServer ss = new SocketServer(port_norm, this);
 		response = SocketClient.sendMsg(validHost, port_norm, msg);
 
 		// port_min
-		msg = "Port 0";
-		response = SocketClient.sendMsg(invalidHost, port_min, msg);
-		response = SocketClient.sendMsg(validHost, port_min, msg);
-
+		/*
+		 * Throws connection problem. port 0 isn't available on my computer msg =
+		 * "Port 0"; ss= new SocketServer(port_min, this); response
+		 * =SocketClient.sendMsg(validHost, port_min, msg);
+		 */
 		// port_min+
 		msg = "Port 1";
-		response = SocketClient.sendMsg(invalidHost, port_min_plus, msg);
+		ss = new SocketServer(port_min_plus, this);
 		response = SocketClient.sendMsg(validHost, port_min_plus, msg);
 
 		// port_max
 		msg = "Port 65535";
-		response = SocketClient.sendMsg(invalidHost, port_max, msg);
+		ss = new SocketServer(port_max, this);
 		response = SocketClient.sendMsg(validHost, port_max, msg);
 
 		// port_max-
 		msg = "Port 65534";
-		response = SocketClient.sendMsg(invalidHost, port_max_minus, msg);
+		ss = new SocketServer(port_max_minus, this);
 		response = SocketClient.sendMsg(validHost, port_max_minus, msg);
 
 		// port_min-
-		msg = "Port -1";
-		response = SocketClient.sendMsg(invalidHost, port_min_minus, msg);
-		response = SocketClient.sendMsg(validHost, port_min_minus, msg);
-
+		/*
+		 * Illegal argument Exception msg = "Port -1"; ss = new
+		 * SocketServer(port_min_minus, this); response =
+		 * SocketClient.sendMsg(validHost, port_min_minus, msg);
+		 */
 		// port_max+
-		msg = "Port 65536";
-		response = SocketClient.sendMsg(invalidHost, port_max_plus, msg);
-		response = SocketClient.sendMsg(validHost, port_max_plus, msg);
+		/*
+		 * Illegal argument Exception msg = "Port -1"; ss = new msg = "Port 65536"; ss =
+		 * new SocketServer(port_max_plus, this); response =
+		 * SocketClient.sendMsg(validHost, port_max_plus, msg);
+		 */
 
 	}
 
