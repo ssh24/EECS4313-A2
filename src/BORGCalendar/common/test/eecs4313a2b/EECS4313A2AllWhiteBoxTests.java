85b4f85bd058f98f040cbf9dac5ee9bf8227f3d2
kirusanth <kirusanth@users.noreply.github.com>
2018-03-02 09:47:47 -0500

    testfolder

---
+++ src/BORGCalendar/common/test/eecs4313a2b/EECS4313A2AllWhiteBoxTests.java
@@ -0,0 +1,14 @@
+package eecs4313a2b;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EECS4313A2AllWhiteBoxTests {
+
+	@Test
+	public void testMinuteString() {
+		fail("Not yet implemented");
+	}
+
+}
