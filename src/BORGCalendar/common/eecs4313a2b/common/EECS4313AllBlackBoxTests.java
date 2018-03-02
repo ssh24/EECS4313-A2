b4f24b4cfa87cfed591984f210cf8d415902eda6
kirusanth <kirusanth@users.noreply.github.com>
2018-03-02 00:31:14 -0500

    updated blackboxtesting

---
+++ src/BORGCalendar/common/eecs4313a2b/common/EECS4313AllBlackBoxTests.java
@@ -0,0 +1,50 @@
+package common;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import net.sf.borg.common.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EECS4313AllBlackBoxTests {
+	@Test
+	public void testMinuteString() {
+		//NINE Weak Normal Equivalence classes
+		//Class 1: Minute/60 = 1 and Minute%60 = 0  (valid input) -- Testing 1 hour
+		assertEquals("1 Hour",DateUtil.minuteString(60));
+		//Class 2: Minute/60 > 1 and Minute%60 = 0 (valid input) --Testing hours more than one (2,3,4....)
+		assertEquals("3 Hours",DateUtil.minuteString(180));
+		//-------------------------------------------------
+		//Class 3: Minute/60 = 1 and Minute%60 > 0 (valid input) --Testing 1 hour with some minutes
+		assertEquals("1 Hour 15 Minutes",DateUtil.minuteString(75));
+		//Class 4: Minute/60 = 1 and Minute%60 = 1 (valid input) --Testing 1 hour with 1 minute
+		assertEquals("1 Hour 1 Minute",DateUtil.minuteString(61));
+		//Class 5: Minute/60 > 1 and Minute%60 > 1 (valid input) --Testing hours more than one with some minutes
+		assertEquals("2 Hours 25 Minutes",DateUtil.minuteString(145));
+		//Class 6: Minute/60 > 1 and Minute%60 = 1 (valid input) --Testing hore more than one with some minutes 
+		assertEquals("2 Hours 1 Minute",DateUtil.minuteString(121));
+		//----------------------------------------------------------
+		//Class 7: Minute/60 = 0 and Minute%60 = 0 (valid input) --Testing 0 minutes
+		assertEquals("0 Minutes",DateUtil.minuteString(0));
+		//Class 8: Minute/60 = 0 and Minute%60 = 1 (valid input) --Testing 1 minute
+		assertEquals("1 Minute",DateUtil.minuteString(1));
+		//Class 9: Minute/60 = 0 and Minute%60 > 0 (valid input) -- Testing minutes that are less than 1 hour : (0 -59 minutes)
+		assertEquals("50 Minutes",DateUtil.minuteString(50));
+
+
+
+
+
+
+			
+
+
+
+
+
+	}
+
+}
