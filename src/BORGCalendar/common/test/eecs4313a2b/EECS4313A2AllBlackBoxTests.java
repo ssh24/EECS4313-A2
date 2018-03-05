f40d3ae797d783bef198c70aed2aa72e59554164
kirusanth <kirusanth@users.noreply.github.com>
2018-03-05 04:26:24 -0500

    version final

--- src/BORGCalendar/common/test/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
+++ src/BORGCalendar/common/test/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -246,6 +246,7 @@ public class EECS4313A2AllBlackBoxTests {
 >>>>>>> 639af8a... model setup
 =======
 	public void testMinuteString() {
+<<<<<<< HEAD
 	
 	//NINE Weak Normal Equivalence classes
 	//Class 1: Minute/60 = 1 and Minute%60 = 0  (valid input) -- Testing 1 hour [Range: [1]]
@@ -269,5 +270,37 @@ public class EECS4313A2AllBlackBoxTests {
 	//Class 9: Minute/60 = 0 and Minute%60 > 1 (valid input) -- Testing minutes that are less than 1 hour [Range: (1,59] minutes]
 	assertEquals("50 Minutes",DateUtil.minuteString(50));
 >>>>>>> 863451b... Final BlackBoxTestCases ONLY
+=======
+
+		// Hour
+		// Class 1: Mins/60 = 1 and Mins%60 = 0 - Testing 1 hour [Range: [1]]
+		assertEquals("1 Hour", DateUtil.minuteString(60));
+		// Class 2: Mins/60 = 1 and Mins%60 = 1 -Testing 1 hour with 1 minute [Range:[1]
+		// hour and [1] minute]
+		assertEquals("1 Hour 1 Minute", DateUtil.minuteString(61));
+		// Class 3: Mins/60 = 1 and Mins%60 > 1 -Testing 1 hour with some minutes
+		// [Range:[1] hour and (1,59] minutes]
+		assertEquals("1 Hour 15 Minutes", DateUtil.minuteString(75));
+		// --------------------------------------------------------------------------------------------------------------
+		// Hours
+		// Class 4: Mins/60 > 1 and Mins%60 = 0 -Testing hours more than one [Range:(1,
+		// infinity) hours]
+		assertEquals("3 Hours", DateUtil.minuteString(180));
+		// Class 5: Mins/60 > 1 and Mins%60 = 1 -Testing hours more than 1 hour with
+		// some minutes [Range:(1,infinity) hours and [1] minute]
+		assertEquals("2 Hours 1 Minute", DateUtil.minuteString(121));
+		// Class 6: Mins/60 > 1 and Mins%60 > 1 -Testing hours more than 1 hour with
+		// some minutes [Range:(1,infinity) hours and (1,59] minutes]
+		assertEquals("2 Hours 25 Minutes", DateUtil.minuteString(145));
+		// -----------------------------------------------------------------------------------------------------------------
+		// Minutes
+		// Class 7: Mins/60 = 0 and Mins%60 = 0 -Testing 0 minutes [Range:[0] minute]
+		assertEquals("0 Minutes", DateUtil.minuteString(0));
+		// Class 8: Mins/60 = 0 and Mins%60 = 1 -Testing 1 minute [Range:[1] minute]
+		assertEquals("1 Minute", DateUtil.minuteString(1));
+		// Class 9: Mins/60 = 0 and Mins%60 > 1 - Testing minutes that are less than 1
+		// hour [Range: (1,59] minutes]
+		assertEquals("50 Minutes", DateUtil.minuteString(50));
+>>>>>>> 0685b32... version final
 	}
 }
